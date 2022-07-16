154f8252b384b5320f5eaf411f654a490c8a4dcb
tmrke <namat9@yandex.ru>
2022-07-16 14:13:38 +0700

    Range task

---
+++ Range/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Range range = new Range(5, 20);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Диапазон от " + range.getFrom() + " до " + range.getTo());
+        System.out.println("Длина диапазона = " + range.getLength());
+
+        System.out.println("Введите число для проверки вхождения в диапазон:");
+        double number = scanner.nextDouble();
+
+        if (range.isInside(number)) {
+            System.out.println("Число в диапазоне");
+        } else {
+            System.out.println("Число не в диапазоне");
+        }
+
+        System.out.println("Измените начало диапазона:");
+        range.setFrom(scanner.nextDouble());
+
+        System.out.println("Измените конец диапазона:");
+        range.setTo(scanner.nextDouble());
+
+        System.out.println("Новый диапазон от " + range.getFrom() + " до " + range.getTo());
+    }
+}
