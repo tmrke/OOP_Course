9ffcc64eb83c4538ad270b66c7c12ab322c14e47
tmrke <namat9@yandex.ru>
2022-07-24 21:34:48 +0700

    Matrix

--- Matrix/src/ru/academits/ageev/matrix/Matrix.java
+++ Matrix/src/ru/academits/ageev/matrix/Matrix.java
@@ -134,30 +134,6 @@ public class Matrix {
         }
     }
 
-    public void add(Matrix matrix){
-
-    }
-
-    private void getReduction(double [][] subMinor, double elemParentMinor) {
-        if (subMinor.length > 1){
-            double [][] tmpMinor = new double[subMinor.length - 1][subMinor[0].length - 1];
-            for (int c = 0; c < subMinor[0].length; c++) {
-                for (int i = 1; i < subMinor.length; i++) {
-                    for (int j = 0; j < subMinor[0].length; j++) {
-                        if (j < c)
-                            tmpMinor[i - 1][j] = subMinor[i][j];
-                        else if (j > c)
-                            tmpMinor[i - 1][j - 1] = subMinor[i][j];
-                    }
-                }
-                double paramForSub = Math.pow(-1,c+2)*subMinor[0][c]*elemParentMinor;
-                getReduction(tmpMinor, paramForSub);
-            }
-        }
-        else
-            this.summ += elemParentMinor * subMinor[0][0];
-    }
-
     @Override
     public String toString() {
         return Arrays.toString(vectorArray);
