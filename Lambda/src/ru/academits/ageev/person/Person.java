aaa155fc2577c26f98af678bb136ce48a8f627df
tmrke <namat9@yandex.ru>
2022-09-22 22:43:12 +0700

    Lambda

--- Lambda/src/ru/academits/ageev/person/Person.java
+++ Lambda/src/ru/academits/ageev/person/Person.java
@@ -1,4 +1,23 @@
 package ru.academits.ageev.person;
 
-public record Person(String name, int age) {
+public class Person {
+    private final String name;
+    private final int age;
+
+    public Person(String name, int age) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Age must be >= 0, age = " + age);
+        }
+
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
--- Lambda/src/ru/academits/ageev/person_main/Main.java
+++ Lambda/src/ru/academits/ageev/person_main/Main.java
@@ -9,8 +9,8 @@ import java.util.stream.DoubleStream;
 public class Main {
     public static void main(String[] args) {
         List<Person> persons = Arrays.asList(
-                new Person("Иван", 10),
-                new Person("Петр", 15),
+                new Person("Иван", 110),
+                new Person("Петр", 115),
                 new Person("Елена", 20),
                 new Person("Ульяна", 30),
                 new Person("Иван", 150)
@@ -19,50 +19,45 @@ public class Main {
         System.out.println("Исходный список:");
         System.out.println(persons);
 
-        List<String> uniqueNames = persons
-                .stream()
-                .map(Person::name)
+        List<String> uniqueNames = persons.stream()
+                .map(Person::getName)
                 .distinct()
                 .toList();
 
         System.out.println("Список уникальных имен: " + uniqueNames);
 
-        String uniqueNamesString = uniqueNames
-                .stream()
+        String uniqueNamesString = uniqueNames.stream()
                 .collect(Collectors.joining(", ", "Уникальные имена: ", "."));
 
         System.out.println(uniqueNamesString);
 
-        List<Person> less18AgePersons = persons
-                .stream()
-                .filter(p -> p.age() < 18)
+        List<Person> less18AgePersons = persons.stream()
+                .filter(p -> p.getAge() < 18)
                 .toList();
 
         System.out.println("Список людей младше 18 лет: " + less18AgePersons);
 
-        OptionalDouble optionalDoubleLess18AgePersonsAverageAge = less18AgePersons
-                .stream()
-                .mapToInt(Person::age)
+        OptionalDouble optionalDoubleLess18AgePersonsAverageAge = less18AgePersons.stream()
+                .mapToInt(Person::getAge)
                 .average();
 
-        assert (optionalDoubleLess18AgePersonsAverageAge.isPresent());
-        double less18AgePersonsAverageAge = optionalDoubleLess18AgePersonsAverageAge.getAsDouble();
-        System.out.println("Средний возраст людей младше 18 лет = " + less18AgePersonsAverageAge);
+        if (optionalDoubleLess18AgePersonsAverageAge.isPresent()) {
+            double less18AgePersonsAverageAge = optionalDoubleLess18AgePersonsAverageAge.getAsDouble();
+            System.out.println("Средний возраст людей младше 18 лет = " + less18AgePersonsAverageAge);
+        }
 
-        Map<String, Double> averageAgesByName = persons
-                .stream()
-                .collect(Collectors.groupingBy(Person::name, Collectors.averagingInt(Person::age)));
+        Map<String, Double> averageAgesByName = persons.stream()
+                .collect(Collectors.groupingBy(Person::getName, Collectors.averagingInt(Person::getAge)));
 
         System.out.println("Map<\"Имена\", \"Средний возраст\"> : " + averageAgesByName);
 
-        String from20to45yearsPersons = persons
-                .stream()
-                .filter(p -> p.age() >= 20 && p.age() <= 45)
-                .sorted(Comparator.comparingInt(Person::age).reversed())
-                .map(Person::name)
+        String from20to45yearsPersonsString = persons.stream()
+                .filter(p -> p.getAge() >= 20 && p.getAge() <= 45)
+                .sorted(Comparator.comparingInt(Person::getAge).reversed())
+                .map(Person::getName)
                 .collect(Collectors.joining(", ", "Люди возрастом от 20 и до 45 лет: ", "."));
 
-        System.out.println(from20to45yearsPersons);
+        System.out.println(from20to45yearsPersonsString);
 
         Scanner scanner = new Scanner(System.in);
 
